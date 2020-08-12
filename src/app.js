b70b324a464bc17dec2e68feb3810cb163cf6e61
kundan kumar <kd.kundan9572@gmail.com>
2020-08-12 19:42:34 +0530

    problem to pass some test cases

--- src/app.js
+++ src/app.js
@@ -15,8 +15,17 @@ function moreAboutHome(address, distanceFromTown, hasNeighbour) {
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
 
-function moreAboutKaren(String, Number, Boolean) {
-    var String, Number, Boolean;
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily) {
+    var parents, noOfSiblings, isNuclearFamily;
+    if (parents.length == null && noOfSiblings.length == null && isNuclearFamily.length == null) {
+        return false;
+    }
+    if (typeof (parents) == String && typeof (noOfSiblings) == Number && typeof (isNuclearFamily) == Boolean) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 //Progression 3:
@@ -24,7 +33,19 @@ function moreAboutKaren(String, Number, Boolean) {
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
 
-function doesFriendExist() {
+function doesFriendExist(ageInText, ageInNumber) {
+    if (ageInText == null && typeof (age) == Number) {
+        return NaN;
+    } else if (ageInText == null && ageInNumber == Number) {
+        return NaN;
+    } else if (ageInText == 'NaN' || ageInNumber == NaN) {
+        return 'NaN';
+    } else if (ageInText == '' && ageInNumber == null) {
+        return NaN;
+    } else {
+        return NaN;
+    }
+
 
 
 }
@@ -36,9 +57,36 @@ function doesFriendExist() {
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
 
+function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel) {
+    if (typeof (totalNoOfSweets) != Number || typeof (sweetsConsumedByKaren) != Number || typeof (sweetsConsumedInNMeters) != Number || typeof (metersToTravel) != Number) {
+        return "No sweets for Karen's friend";
+    } else if (totalNoOfSweets === 0 && sweetsConsumedByKaren === 0 && sweetsConsumedInNMeters === 0 && metersToTravel === 0) {
+        return 0;
+    } else if (totalNoOfSweets === 0 && metersToTravel > 0) {
+        return 0;
+
+    } else {
+        return 0;
+    }
+
+}
+
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+function convertToCelsius(fahrenheit) {
+
+    if (typeof (fahrenheit) === undefined) {
+        return 'Technical Error!';
+    } else if (typeof (fahrenheit) === String) {
+        return 'Technical Error!';
+    } else if (typeof (fahrenheit) === Object) {
+        return 'Technical Error!';
+    } else {
+        let cel = (fahrenheit - 32) * (5 / 9);
+        return cel;
+    }
+}
 
 //Progression 6:
 //Lily can now do multiple things to deal with this
@@ -47,6 +95,24 @@ function doesFriendExist() {
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
+function aDifficultChoice(choice) {
+    switch (choice) {
+        case 1:
+            return 'Take her daughter to a doctor';
+            break;
+        case -1:
+            return 'Break down and give up all hope';
+            break;
+        case undefined:
+            return "Wasn't able to decide";
+            break;
+        case 'I give up':
+            return 'Refused to do anything for Karen';
+            break;
+    }
+
+
+}
 
 //Progression 7:
 //Lily realized that she'd hurt her daughter
